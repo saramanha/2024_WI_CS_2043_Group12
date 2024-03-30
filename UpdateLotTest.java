e046a2cfaf322898e1b49cf26944a3288a5bdd2e
pwilson3 <pwilson3@unb.ca>
2024-03-30 17:22:37 -0300

    final test push for implementation

--- UpdateLotTest.java
+++ UpdateLotTest.java
@@ -8,7 +8,7 @@ public class UpdateLotTest{
     public static void main(String[] args){
         String[][] results = UpdateLot.getLotData();
         for(int i = 0; i < 40; i++){
-            if (results[i][1].compareTo("FALSE") == 0) System.out.println(results[i][0] + "| empty");
+            if (results[i][1].compareTo("false") == 0) System.out.println(results[i][0] + "| empty");
             else {
                 for (int j = 0; j < 5; j++){
                     System.out.println(results[i][j] + "| ");
